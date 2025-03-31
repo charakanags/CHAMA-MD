fb722573e2a993786f721b336192ebae82037bc8
charakanags <repomax074@gmail.com>
2025-03-31 21:32:11 +0530

    Update ytvdl.js

--- plugins/ytvdl.js
+++ plugins/ytvdl.js
@@ -6,7 +6,7 @@ const { ytsearch, ytmp3, ytmp4 } = require('@dark-yasiya/yt-dl.js');
 
 cmd({ 
     pattern: "mp4", 
-    alias: ["video""v"], 
+    alias: ["video", "v"], 
     react: "🎥", 
     desc: "Download YouTube video", 
     category: "main", 
